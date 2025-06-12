0b1896b4c2bcab4200e999c90d52d54e3cf65997
fatema123elfiky <fatmaelfeky922@gmail.com>
2025-06-13 00:20:29 +0300

    The start of priority queue

---
+++ priority queue/PriorityHeap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Fatma on 6/12/2025.
+//
+
+#include "PriorityHeap.h"
---
+++ priority queue/PriorityHeap.h
@@ -0,0 +1,17 @@
+//
+// Created by Fatma on 6/12/2025.
+//
+
+#ifndef SELF_DATASTRUCTURE_OWN_IMPLEMENTATION_PRIORITYHEAP_H
+#define SELF_DATASTRUCTURE_OWN_IMPLEMENTATION_PRIORITYHEAP_H
+
+template<typename T>
+class PriorityHeap {
+private:
+    T *array{};
+    int size{};
+    int capacity{1000};
+};
+
+
+#endif //SELF_DATASTRUCTURE_OWN_IMPLEMENTATION_PRIORITYHEAP_H
